417c884f29762ee6f12c8b3641f90fc71ddc9952
alphazm <144003097+alphazm@users.noreply.github.com>
2023-09-27 00:11:18 +0800

    Create Receipt.java

---
+++ Receipt.java
@@ -0,0 +1,53 @@
+package POS;
+
+public class Receipt {
+
+
+    private int receiptID;
+    private long second;
+    private double price;
+    private int quantity;
+
+    public Receipt(int receiptID, long second,int quantity,double price) {
+        this.receiptID = receiptID;
+        this.second = second;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
+
+    public int getReceiptID() {
+        return receiptID;
+    }
+
+    public void setReceiptID(int receiptID) {
+        this.receiptID = receiptID;
+    }
+
+    public long getSecond() {
+        return second;
+    }
+
+    public void setSecond(long second) {
+        this.second = second;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    
+}
